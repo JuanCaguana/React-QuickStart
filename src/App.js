cdd9ba07a65b91e1def5a2aa7d1f92c229b16d19
Jaun C <jmcaguanahue@gmail.com>
2023-09-25 17:01:08 -0500

    Displaying data

--- src/App.js
+++ src/App.js
@@ -21,6 +21,29 @@ function AboutPage() {
   );
 }
 
+//displaying data
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
@@ -36,6 +59,10 @@ function App() {
 
       <div>
         <AboutPage/>
+      </div>
+
+      <div>
+        <Profile/>
       </div>
 
       <img className="avatar" />
